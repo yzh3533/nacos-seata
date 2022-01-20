2df19e701b4ca7d7c630bde90112d373c69747ed
youzhenxun <youzhenxun@gmail.com>
2022-01-20 18:38:44 +0800

    微调

--- user-server/src/main/java/com/example/userserver/UserServerApplication.java
+++ user-server/src/main/java/com/example/userserver/UserServerApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages  = {"com.example.*"})
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.example.*"})
 @ComponentScan(basePackages = {"com.example.*"})
 @ServletComponentScan
--- user-server/src/main/java/com/example/userserver/service/UserService.java
+++ user-server/src/main/java/com/example/userserver/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.userserver.service;
 
 import com.example.feignclient.OrderClient;
+import com.example.orderserver.service.OrderService;
 import com.example.userserver.dao.UserDao;
 import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
@@ -18,11 +19,14 @@ public class UserService {
     private OrderClient orderClient;
     @Autowired
     private UserDao dao;
+    @Autowired
+    private OrderService orderService;
 
     @GlobalTransactional(timeoutMills = 300000, name = "dubbo-gts-seata-example")
     public void updateAccount(BigDecimal price, Long id,Boolean flag){
         log.info("开始全局事务，XID = " + RootContext.getXID());
-        orderClient.create();
+//        orderClient.create();
+        orderService.createOrder(1L,2L);
         if(flag) {
             throw new RuntimeException("测试抛异常后，分布式事务回滚！");
         }
